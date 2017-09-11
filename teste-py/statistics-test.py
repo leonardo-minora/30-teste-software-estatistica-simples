5369b918e8bf20a2fb4e729385124ba02aef02e5
Késsia Castro <kessiacastro@gmail.com>
2017-09-11 16:15:48 -0300

    finalizando

--- teste-py/statistics-test.py
+++ teste-py/statistics-test.py
@@ -9,6 +9,9 @@ class StatisticsTest (unittest.TestCase):
     def test_is_not_a_vector(self):
         with self.assertRaises(TypeError):
             estatisticas(['a', 'b'])
+    
+    def test_size_bigger_than_zero(self):
+        self.assertEquals(estatisticas([1,2]), {'maior': 2, 'menor': 1, 'media': 1.5, 'tamanho': 2})
 
 if __name__ == '__main__':
     unittest.main()
--- teste-py/statistics.py
+++ teste-py/statistics.py
@@ -1,9 +1,13 @@
+from __future__ import division
+
 
 vec = []
 
 def estatisticas(vector):
-    if (type(vector) is list):
+    if (type(vector) is list and len(vector) > 0):
         for element in vector:
             if (type(element) is not int):
                 raise(TypeError)
-    return True
+        return {'maior': max(vector), 'menor': min(vector), 'media': sum(vector)/len(vector), 'tamanho': len(vector) }
+    else:
+        raise(ValueError)
